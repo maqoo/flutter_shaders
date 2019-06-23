fcf39c2f355a09241e5514f0105a98b6f29c4088
Maciej Stramski <mstramski@gmail.com>
2019-06-23 20:34:49 +0200

    Blurring list view

--- lib/blurring.dart
+++ lib/blurring.dart
@@ -6,17 +6,36 @@ class BlurringPage extends StatefulWidget {
 }
 
 class BlurringPageState extends State<BlurringPage> {
+  Widget get _background => Container(
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            radius: 0.8,
+            colors: <Color>[Colors.limeAccent, Colors.deepOrangeAccent],
+          ),
+        ),
+      );
+
+  Widget get _list => ListView.builder(
+        itemCount: 10,
+        itemBuilder: (BuildContext context, int index) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+                width: 100,
+                height: 28,
+                color: Colors.red),
+          );
+        },
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Blurring'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
+      body: Stack(
+        children: <Widget>[_background, _list],
       ),
     );
   }
