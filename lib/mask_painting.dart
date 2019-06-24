f4f36b4ff49bde8c25216675111743de501867dc
Maciej Stramski <mstramski@gmail.com>
2019-06-25 00:34:39 +0200

    Oh the struggle ... Masking WIP

---
+++ lib/mask_painting.dart
@@ -0,0 +1,46 @@
+import 'dart:ui' as ui;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart' as rendering;
+import 'package:flutter/widgets.dart';
+
+class MaskPainting extends SingleChildRenderObjectWidget {
+  MaskPainting(Widget child, {this.image})
+      : assert(child != null),
+        super(child: child);
+
+  final ui.Image image;
+
+  @override
+  RenderObject createRenderObject(BuildContext context) => MaskPaintingFilter(image);
+}
+
+class MaskPaintingFilter extends rendering.RenderProxyBox {
+  MaskPaintingFilter(this.image) : _maskPaint = Paint()..blendMode = BlendMode.srcIn;
+
+  final Paint _clearPaint = Paint();
+  final Paint _maskPaint;
+
+  final ui.Image image;
+
+  @override
+  bool get alwaysNeedsCompositing => child != null;
+
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    if (child == null) {
+      return;
+    }
+
+    final Rect rect = offset & child.size;
+    _maskPaint.shader = ImageShader(
+        image, TileMode.clamp, TileMode.clamp, Matrix4.identity().storage);
+
+//    _maskPaint.shader = SweepGradient(colors: <Color>[Colors.white, Colors.black], tileMode: TileMode.repeated).createShader(rect);
+
+    context.canvas.saveLayer(rect, _clearPaint);
+    context.paintChild(child, offset);
+    context.canvas.drawRect(rect, _maskPaint);
+    context.canvas.restore();
+  }
+}
--- lib/masking.dart
+++ lib/masking.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
+import 'package:flutter_shaders/mask_painting.dart';
 
 class MaskingPage extends StatefulWidget {
   @override
@@ -6,6 +10,23 @@ class MaskingPage extends StatefulWidget {
 }
 
 class MaskingPageState extends State<MaskingPage> {
+  Widget get _background => Container(
+        width: 400,
+        height: 400,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: <Color>[
+              Colors.limeAccent,
+              Colors.deepOrangeAccent,
+              Colors.blue,
+              Colors.green
+            ],
+          ),
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,9 +36,36 @@ class MaskingPageState extends State<MaskingPage> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
+          children: <Widget>[
+            FutureBuilder<ui.Image>(
+              future: _getAssetImage('assets/images/mask.png'),
+              builder:
+                  (BuildContext context, AsyncSnapshot<ui.Image> snapshot) {
+                if (snapshot.hasData) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.done:
+                      return MaskPainting(_background, image: snapshot.data);
+                    default:
+                      return SizedBox.shrink();
+                  }
+                }
+
+                return SizedBox.shrink();
+              },
+            )
+          ],
         ),
       ),
     );
+  }
+
+  Future<ui.Image> _getAssetImage(String assetPath) {
+    Completer<ui.Image> completer = new Completer<ui.Image>();
+
+    new AssetImage(assetPath).resolve(new ImageConfiguration()).addListener(
+        ImageStreamListener(
+            (ImageInfo info, bool _) => completer.complete(info.image)));
+
+    return completer.future;
   }
 }
